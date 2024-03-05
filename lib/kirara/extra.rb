341ec10abe2e5712a031a37a0189c0b06945b6ca
aa <37037844+Saisui@users.noreply.github.com>
2024-03-05 18:30:38 +0800

    fix script_ls

--- lib/kirara/extra.rb
+++ lib/kirara/extra.rb
@@ -3,7 +3,7 @@ class DocElm
   def LS ls_heredoc
     echo LiveScript.compile ls_heredoc
   end
-  def script_ls
+  def script_ls ls_heredoc
     script do
       echo LiveScript.compile ls_heredoc
     end
