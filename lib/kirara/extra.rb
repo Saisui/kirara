3d26d35ba1a284ad639ac4993da4c7819ed531ce
aa <37037844+Saisui@users.noreply.github.com>
2024-03-05 19:42:47 +0800

    + templ def

--- lib/kirara/extra.rb
+++ lib/kirara/extra.rb
@@ -10,41 +10,7 @@ class DocElm
   end
 end
 
-# def templ sym, tagname=nil, **kws, &pbody
-#   pbody ||= method(sym).to_proc
-#   DocElm.define_method sym do |*as, **kats, &blk|
-#     mth = ->{ pbody.call(*as, **kats, &blk) }
-#     @children << [DocElm.new(tagname).instance_eval(&mth), :templ]
-#   end
-# end
-def templ sym
+def templ sym, &pbody
   pbody ||= method(sym).to_proc
-  DocElm.define_method sym do
-    @children << [DocElm.new[&pbody], :templ]
-  end
-end
-
-templ :my do |name|
-  li name: name
-  li name: name
-end
-templ def my
-  li name: name
-  li name: name
-end
-templ def my
-  li
-  li
-end
-
-EL.li do
-  my "black"
-end
-
-class DocElm
-  def my
-    li
-    li
-    li
-  end
+  DocElm.define_method :my, &pbody
 end
